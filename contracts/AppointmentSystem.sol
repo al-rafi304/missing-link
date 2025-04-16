d0cfc7736ae35e95866d16ac9a5ca60d744abdcb
Mohammad Al Rafi <arafi304@gmail.com>
2025-04-16 23:17:46 +0600

    feat: implement appointment system

--- contracts/AppointmentSystem.sol
+++ contracts/AppointmentSystem.sol
@@ -1,3 +1,74 @@
-// TODO: 
-//     - function bookAppointment() payable reporterOnly
-//     - function getInvestigatorAppointments()
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+import "contracts/UserManagement.sol";
+import "contracts/MissingRegistry.sol";
+
+contract AppointmentSystem {
+
+    uint public appointmentFee = 5 ether;
+
+    uint public appointmentId = 1;
+    struct Appointment {
+        uint id;
+        uint caseId;
+        address Investigator;
+        address Reporter;
+        uint timestamp;
+    }
+
+    mapping(address => Appointment[]) public appointments;
+    UserManagement public userManager;
+    MissingRegistry public missingRegistry;
+
+    constructor(address _userManagement, address _missingRegistry) {
+        userManager = UserManagement(_userManagement);
+        missingRegistry = MissingRegistry(_missingRegistry);
+    }
+
+    modifier onlyReporter() {
+        require(userManager.isReporter(msg.sender), "Only reporter can perform this action");
+        _;
+    }
+
+    modifier onlyInvestigator() {
+        require(userManager.isInvestigator(msg.sender), "Only investigator can perform this action");
+        _;
+    }
+    function bookAppointment(uint _caseId, address _investigator, uint _timestamp) onlyReporter external payable {
+        require(userManager.isInvestigator(_investigator), "Invalid investigator address");
+        require(msg.value >= appointmentFee, "Insufficient payment for appointment");
+
+        Appointment[] memory aps = getAppointments(_investigator);
+        for (uint i=0; i < aps.length; i++){
+            if(_timestamp >= aps[i].timestamp && _timestamp<= aps[i].timestamp + 600) {
+                revert("Appintment already exists for this timeslot");
+            }
+        }
+
+        appointments[_investigator].push(Appointment({
+            id: appointmentId,
+            caseId: _caseId,
+            Investigator: _investigator, 
+            Reporter: msg.sender, 
+            timestamp: _timestamp
+        }));
+        appointmentId++;
+    }
+
+    function getAppointments(address investigator) public view returns (Appointment[] memory) {
+        return appointments[investigator];
+    }
+
+    function getAppointmentDetails(address _investigator, uint _appointmentId) external view returns (Appointment memory) {
+        for (uint i = 0; i < appointments[_investigator].length; i++) 
+        {
+            if (appointments[_investigator][i].id == _appointmentId) {
+                return appointments[_investigator][i];
+            }
+        }
+
+        revert("Appointment not found");
+    }
+}
